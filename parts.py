dc57deb646f8a7ad9fa4002a3fd99394c602dd8b
Alvyn Abranches <alvynabranches@gmail.com>
2020-06-14 23:32:04 +0530

    14062020v3

---
+++ parts.py
@@ -0,0 +1,49 @@
+class Product:
+    def __init__(self, product_id, product_name):
+        self.product_id = product_id
+        self.product_name = product_name
+
+    def __str__(self):
+        return f"{self.product_name}"
+
+class Motherboard(Product):
+    def __init__(self, product_id, product_name):
+        super().__init__(product_id, product_name)
+
+    def __str__(self):
+        return super().__str__()
+
+class Processor(Product):
+    def __init__(self, product_id, product_name):
+        super().__init__(product_id, product_name)
+    
+    def __str__(self):
+        return super().__str__()
+
+class GPU(Product):
+    def __init__(self, product_id, product_name):
+        super().__init__(product_id, product_name)
+
+    def __str__(self):
+        return super().__str__()
+
+class TPU(Product):
+    def __init__(self, product_id, product_name):
+        super().__init__(product_id, product_name)
+
+    def __str__(self):
+        return super().__str__()
+
+class PowerSupply(Product):
+    def __init__(self, product_id, product_name):
+        super().__init__(product_id, product_name)
+
+    def __str__(self):
+        return super().__str__()
+
+class RAM(Product):
+    def __init__(self, product_id, product_name):
+        super().__init__(product_id, product_name)
+
+    def __str__(self):
+        return super().__str__()
