8434ac3783974b6b527e47e49a5f255b88e69052
Alvyn Abranches <alvynabranches@gmail.com>
2020-06-15 01:11:12 +0530

    14062020v5

--- parts.py
+++ parts.py
@@ -1,5 +1,5 @@
 class Product:
-    def __init__(self, product_id, product_name):
+    def __init__(self, product_id, product_name, product_company):
         self.product_id = product_id
         self.product_name = product_name
 
@@ -7,43 +7,43 @@ class Product:
         return f"{self.product_name}"
 
 class Motherboard(Product):
-    def __init__(self, product_id, product_name):
-        super().__init__(product_id, product_name)
+    def __init__(self, product_id, product_name, product_company):
+        super().__init__(product_id, product_name, product_company)
 
     def __str__(self):
         return super().__str__()
 
 class Processor(Product):
-    def __init__(self, product_id, product_name):
-        super().__init__(product_id, product_name)
+    def __init__(self, product_id, product_name, product_company):
+        super().__init__(product_id, product_name, product_company)
     
     def __str__(self):
         return super().__str__()
 
 class GPU(Product):
-    def __init__(self, product_id, product_name):
-        super().__init__(product_id, product_name)
+    def __init__(self, product_id, product_name, product_company):
+        super().__init__(product_id, product_name, product_company)
 
     def __str__(self):
         return super().__str__()
 
 class TPU(Product):
-    def __init__(self, product_id, product_name):
-        super().__init__(product_id, product_name)
+    def __init__(self, product_id, product_name, product_company):
+        super().__init__(product_id, product_name, product_company)
 
     def __str__(self):
         return super().__str__()
 
 class PowerSupply(Product):
-    def __init__(self, product_id, product_name):
-        super().__init__(product_id, product_name)
+    def __init__(self, product_id, product_name, product_company):
+        super().__init__(product_id, product_name, product_company)
 
     def __str__(self):
         return super().__str__()
 
 class RAM(Product):
-    def __init__(self, product_id, product_name):
-        super().__init__(product_id, product_name)
+    def __init__(self, product_id, product_name, product_company):
+        super().__init__(product_id, product_name, product_company)
 
     def __str__(self):
         return super().__str__()
